83503c74c9342f67f5767340252249f02cade9dd
Steve Persch <steve.persch@pantheon.io>
2016-06-30 18:46:56 -0500

    Removing the migration pieces

--- tests/circle/setup-d8-repo.sh
+++ tests/circle/setup-d8-repo.sh
@@ -11,17 +11,11 @@ composer config repositories.search_api_pantheon vcs git@github.com:stevector/se
 composer require  drupal/search_api_pantheon:dev-master#$CIRCLE_SHA1
 
 
-# Bring in Migrate-related contrib modules.
-composer require drupal/migrate_plus:8.2.x-dev --prefer-dist
-composer require drupal/migrate_tools:8.2.x-dev --prefer-dist
-composer require drupal/migrate_upgrade:8.2.x-dev --prefer-dist
 
 
 composer require drupal/search_api_page:8.1.x-dev --prefer-dist
 # Make sure submodules are not committed.
-rm -rf modules/migrate_plus/.git/
-rm -rf modules/migrate_tools/.git/
-rm -rf modules/migrate_upgrade/.git/
+
 rm -rf modules/search_api_solr/.git/
 rm -rf modules/search_api/.git/
 rm -rf modules/search_api_page/.git/
@@ -32,10 +26,6 @@ mkdir modules/search_api_pantheon
 # @todo, need better way to setup module. What is the composer way of doing this?
 cp  ../../search_api_pantheon.* modules/search_api_pantheon/
 
-
-# Set up the settings.php connection to the source database.
-cp ../fixtures/settings.migrate-on-pantheon.php sites/default/
-cat ../fixtures/settings.php.addition >> sites/default/settings.php
 
 # Make a git commit
 git config user.email "$GitEmail"
--- tests/circle/verify-migration-succeeded.sh
+++ tests/circle/verify-migration-succeeded.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-export BEHAT_PARAMS='{"extensions" : {"Behat\\MinkExtension" : {"base_url" : "http://'$TERMINUS_ENV'-'$TERMINUS_SITE'.pantheonsite.io"} }}'
-cd ~/search_api_pantheon/tests/circle && ./../../vendor/bin/behat --config=behat/config.yml
-
-
-
 
 # Create a drush alias file so that Behat tests can be executed against Pantheon.
 terminus sites aliases
@@ -12,5 +7,5 @@ terminus sites aliases
 echo "\$options['strict'] = 0;" >> ~/.drush/pantheon.aliases.drushrc.php
 export BEHAT_PARAMS='{"extensions" : {"Behat\\MinkExtension" : {"base_url" : "http://'$TERMINUS_ENV'-'$TERMINUS_SITE'.pantheonsite.io/"}, "Drupal\\DrupalExtension" : {"drush" :   {  "alias":  "@pantheon.'$TERMINUS_SITE'.'$TERMINUS_ENV'" }}}}'
 # Make sure the site is accessible over the web before making requests to it with Behat.
-curl http://$TERMINUS_ENV-$TERMINUS_SITE.pantheonsite.io/
-cd ~/search_api_pantheon && ./vendor/bin/behat      --config=behat/behat-pantheon.yml      behat/features/solr
+#curl http://$TERMINUS_ENV-$TERMINUS_SITE.pantheonsite.io/
+cd ../../ && ./vendor/bin/behat      --config=behat/behat-pantheon.yml      behat/features/solr
