7efcc99de7cd5e8efefdee7f4bcc44a0cbc66d75
Steve Persch <stevector@users.noreply.github.com>
2016-06-30 14:14:44 -0500

    Update verify-migration-succeeded.sh

--- tests/circle/verify-migration-succeeded.sh
+++ tests/circle/verify-migration-succeeded.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export BEHAT_PARAMS='{"extensions" : {"Behat\\MinkExtension" : {"base_url" : "http://'$TERMINUS_ENV'-'$TERMINUS_SITE'.pantheonsite.io"} }}'
-cd ~/migrate_pantheon/tests/circle && ./../../vendor/bin/behat --config=behat/config.yml
+cd ~/search_api_pantheon/tests/circle && ./../../vendor/bin/behat --config=behat/config.yml
