fec8aa8aaa8e9bebbe8066f71bce7e5280e24a8e
Steve Persch <steve.persch@pantheon.io>
2016-06-30 18:17:25 -0500

    More behat config. Form alter

--- tests/circle/configure-migrations.sh
+++ tests/circle/configure-migrations.sh
@@ -36,6 +36,7 @@ terminus drush "cache-rebuild"
 
 
 terminus drush "en -y search_api_pantheon search_api_solr search_api_page search_api"
+terminus drush "cache-rebuild"
 terminus site set-connection-mode --mode=sftp
 terminus drush "cache-rebuild"
 terminus drush search-api-pantheon-schema-post
