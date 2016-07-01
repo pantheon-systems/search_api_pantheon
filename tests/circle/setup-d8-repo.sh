2986ea6dbf4c53ca0f9cfda8efe7f2a4817575a8
Steve Persch <steve.persch@pantheon.io>
2016-06-30 20:01:35 -0500

    Fixing push

--- tests/circle/setup-d8-repo.sh
+++ tests/circle/setup-d8-repo.sh
@@ -2,7 +2,6 @@
 
 # Bring the code down to Circle so that modules can be added via composer.
 git clone $(terminus site connection-info --field=git_url) drupal8
-#git clone $(terminus site connection-info --field=git_url) drupal8 --branch=$TERMINUS_ENV
 cd drupal8
 git checkout -b $TERMINUS_ENV
 
@@ -11,26 +10,25 @@ composer config repositories.drupal composer https://packagist.drupal-composer.o
 
 
 # This is a section that applies a patch to composer.json... So that Composer
-# can apply a patch to drupal/migrate_upgrade from
-# https://www.drupal.org/node/2751151.
+# can apply a patches. This step should not be necessary. It should be enough
+# to require drupal/search_api_pantheon which can then define patches.
 # I hope you find the Rube Goldberg absurdity of this section as enjoyable
 # as I do.
 cp ../../../patches/core-composer.patch .
 git apply core-composer.patch
 rm core-composer.patch
 composer require cweagans/composer-patches --prefer-dist
-
 composer require solarium/solarium:3.6.*
 composer require drupal/search_api:8.1.x-dev --prefer-dist
 composer require drupal/search_api_solr:8.1.x-dev --prefer-dist
 composer require drupal/search_api_page:8.1.x-dev --prefer-dist
-# Make sure submodules are not committed.
+
 
 
 composer config repositories.search_api_pantheon vcs git@github.com:stevector/search_api_pantheon.git
 composer require  drupal/search_api_pantheon:dev-master#$CIRCLE_SHA1
 
-
+# Make sure submodules are not committed.
 rm -rf modules/search_api_solr/.git/
 rm -rf modules/search_api/.git/
 rm -rf modules/search_api_page/.git/
@@ -43,4 +41,4 @@ git config user.email "$GitEmail"
 git config user.name "Circle CI Migration Automation"
 git add .
 git commit -m 'Result of build step'
-git push
+git push --set-upstream origin $TERMINUS_ENV
