692c3d36d856cdb6d622a3e022921c8fb9111738
Steve Persch <steve.persch@pantheon.io>
2016-07-01 14:25:40 -0500

    renaming scripts

--- tests/circle/enable-modules.sh
+++ tests/circle/enable-modules.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-
-
-terminus drush "cache-rebuild"
 terminus drush "cache-rebuild"
 terminus drush "en -y search_api_pantheon search_api_solr search_api_page search_api"
-terminus drush "cache-rebuild"
+# @todo, would this test benefit from exporting/committing config?
 terminus site set-connection-mode --mode=sftp
 terminus drush "cache-rebuild"
+# @todo, make schema posting part of the install step for search_api_pantheon.
 terminus drush search-api-pantheon-schema-post
