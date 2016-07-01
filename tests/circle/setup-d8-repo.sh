b2da900f8ca5325867141cf669341dca121686ff
Steve Persch <steve.persch@pantheon.io>
2016-06-30 19:52:24 -0500

    Moving composer require of search_api_pantheon to end

--- tests/circle/setup-d8-repo.sh
+++ tests/circle/setup-d8-repo.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Bring the code down to Circle so that modules can be added via composer.
-git clone $(terminus site connection-info --field=git_url) drupal8 --branch=$TERMINUS_ENV
+git clone $(terminus site connection-info --field=git_url) drupal8
+#git clone $(terminus site connection-info --field=git_url) drupal8 --branch=$TERMINUS_ENV
 cd drupal8
+git checkout -b $TERMINUS_ENV
 
 # Tell Composer where to find packages.
 composer config repositories.drupal composer https://packagist.drupal-composer.org
@@ -18,16 +20,16 @@ git apply core-composer.patch
 rm core-composer.patch
 composer require cweagans/composer-patches --prefer-dist
 
-
-composer config repositories.search_api_pantheon vcs git@github.com:stevector/search_api_pantheon.git
-composer require  drupal/search_api_pantheon:dev-master#$CIRCLE_SHA1
-
-
 composer require solarium/solarium:3.6.*
 composer require drupal/search_api:8.1.x-dev --prefer-dist
 composer require drupal/search_api_solr:8.1.x-dev --prefer-dist
 composer require drupal/search_api_page:8.1.x-dev --prefer-dist
 # Make sure submodules are not committed.
+
+
+composer config repositories.search_api_pantheon vcs git@github.com:stevector/search_api_pantheon.git
+composer require  drupal/search_api_pantheon:dev-master#$CIRCLE_SHA1
+
 
 rm -rf modules/search_api_solr/.git/
 rm -rf modules/search_api/.git/
