674222f78452b253e7195dfa5477c326b254532a
Steve Persch <stevector@users.noreply.github.com>
2016-12-16 10:44:02 -0600

    trying to use global TERMINUS_SITE and TERMINUS_ENV

--- tests/circle-scripts/enable-modules.sh
+++ tests/circle-scripts/enable-modules.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-terminus drush $TERMINUS_SITE.$TERMINUS_ENV "cache-rebuild"
-terminus drush $TERMINUS_SITE.$TERMINUS_ENV "pml"
+terminus drush "cache-rebuild"
+terminus drush "pml"
 # Uninstall core search to reduce confusion in the UI.
-terminus drush $TERMINUS_SITE.$TERMINUS_ENV "pm-uninstall search -y"
-terminus drush $TERMINUS_SITE.$TERMINUS_ENV "en -y search_api_pantheon search_api_solr search_api_page search_api"
+terminus drush "pm-uninstall search -y"
+terminus drush "en -y search_api_pantheon search_api_solr search_api_page search_api"
 # @todo, would this test benefit from exporting/committing config?
-terminus connection:set $TERMINUS_SITE.$TERMINUS_ENV sftp
+terminus connection:set sftp
