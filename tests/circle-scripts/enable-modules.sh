adc5b0e88d9f0cf141bf140e3d051cdcbb8f76e2
Steve Persch <steve.persch@pantheon.io>
2016-12-21 13:40:59 -0600

    Revert "trying to use global TERMINUS_SITE and TERMINUS_ENV"
    This reverts commit 674222f78452b253e7195dfa5477c326b254532a.

--- tests/circle-scripts/enable-modules.sh
+++ tests/circle-scripts/enable-modules.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-terminus drush "cache-rebuild"
-terminus drush "pml"
+terminus drush $TERMINUS_SITE.$TERMINUS_ENV "cache-rebuild"
+terminus drush $TERMINUS_SITE.$TERMINUS_ENV "pml"
 # Uninstall core search to reduce confusion in the UI.
-terminus drush "pm-uninstall search -y"
-terminus drush "en -y search_api_pantheon search_api_solr search_api_page search_api"
+terminus drush $TERMINUS_SITE.$TERMINUS_ENV "pm-uninstall search -y"
+terminus drush $TERMINUS_SITE.$TERMINUS_ENV "en -y search_api_pantheon search_api_solr search_api_page search_api"
 # @todo, would this test benefit from exporting/committing config?
-terminus connection:set sftp
+terminus connection:set $TERMINUS_SITE.$TERMINUS_ENV sftp
