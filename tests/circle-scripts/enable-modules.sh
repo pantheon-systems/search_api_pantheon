7b48147b63912ef86bc0d3dffd621eb852b02f99
Steve Persch <stevector@users.noreply.github.com>
2016-12-27 10:03:51 -0600

    Update enable-modules.sh

--- tests/circle-scripts/enable-modules.sh
+++ tests/circle-scripts/enable-modules.sh
@@ -2,10 +2,10 @@
 
 set -x
 
-terminus drush $SITE_ENV "cache-rebuild"
-terminus drush $SITE_ENV "pml"
+terminus drush $SITE_ENV -- cache-rebuild
+terminus drush $SITE_ENV -- pml
 # Uninstall core search to reduce confusion in the UI.
-terminus drush $SITE_ENV "pm-uninstall search -y"
-terminus drush $SITE_ENV "en -y search_api_pantheon search_api_solr search_api_page search_api"
+terminus drush $SITE_ENV -- pm-uninstall search -y
+terminus drush $SITE_ENV -- en -y search_api_pantheon search_api_solr search_api_page search_api
 # @todo, would this test benefit from exporting/committing config?
 terminus connection:set $SITE_ENV sftp
