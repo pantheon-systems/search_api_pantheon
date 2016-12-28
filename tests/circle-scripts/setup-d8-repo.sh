fc2897f7f4425e8db5154735eedbd97687ccdcf6
Steve Persch <stevector@users.noreply.github.com>
2016-12-28 13:08:41 -0600

    Installing search_api, search_api_solr before search_api_pantheon

--- tests/circle-scripts/setup-d8-repo.sh
+++ tests/circle-scripts/setup-d8-repo.sh
@@ -9,14 +9,15 @@ git checkout -b $TERMINUS_ENV
 
 # Tell Composer where to find packages.
 composer config repositories.drupal composer https://packages.drupal.org/8
-composer config repositories.search_api_pantheon vcs git@github.com:pantheon-systems/search_api_pantheon.git
-composer require drupal/search_api_pantheon:dev-8.x-1.x#$CIRCLE_SHA1
-composer require drupal/search_api_page:1.x-dev
 
 # These two lines are necessary only to force dev installs,
 # otherwise the latest releases would be used.
-composer require drupal/search_api_solr:1.x-dev --prefer-dist
 composer require drupal/search_api:1.x-dev --prefer-dist
+composer require drupal/search_api_solr:1.x-dev --prefer-dist
+
+composer config repositories.search_api_pantheon vcs git@github.com:pantheon-systems/search_api_pantheon.git
+composer require drupal/search_api_pantheon:dev-8.x-1.x#$CIRCLE_SHA1
+composer require drupal/search_api_page:1.x-dev
 
 # Make sure submodules are not committed.
 rm -rf modules/search_api_solr/.git/
