e77708731d843caac68252e78ee1102d4ab7d001
Steve Persch <stevector@users.noreply.github.com>
2016-12-28 13:22:36 -0600

    using terminus composer

--- tests/circle-scripts/setup-d8-repo.sh
+++ tests/circle-scripts/setup-d8-repo.sh
@@ -3,30 +3,25 @@
 set -x
 
 # Bring the code down to Circle so that modules can be added via composer.
-git clone $(terminus connection:info $SITE_ENV --field=git_url) drupal8
-cd drupal8
-git checkout -b $TERMINUS_ENV
+#git clone $(terminus connection:info $SITE_ENV --field=git_url) drupal8
+#cd drupal8
+#git checkout -b $TERMINUS_ENV
+terminus connection:set $SITE_ENV sftp
 
 # Tell Composer where to find packages.
-composer config repositories.drupal composer https://packages.drupal.org/8
+terminus composer $SITE_ENV -- config repositories.drupal composer https://packages.drupal.org/8
 
 # These two lines are necessary only to force dev installs,
 # otherwise the latest releases would be used.
-composer require drupal/search_api:1.x-dev --prefer-dist
-composer require drupal/search_api_solr:1.x-dev --prefer-dist
+terminus composer $SITE_ENV -- require drupal/search_api:1.x-dev --prefer-dist
+terminus composer $SITE_ENV -- require drupal/search_api_solr:1.x-dev --prefer-dist
 
-composer config repositories.search_api_pantheon vcs git@github.com:pantheon-systems/search_api_pantheon.git
-composer require drupal/search_api_pantheon:dev-8.x-1.x#$CIRCLE_SHA1
-composer require drupal/search_api_page:1.x-dev
+terminus composer $SITE_ENV -- require drupal/search_api_page:1.x-dev
+terminus composer $SITE_ENV -- config repositories.search_api_pantheon vcs git@github.com:pantheon-systems/search_api_pantheon.git
+terminus composer $SITE_ENV -- require drupal/search_api_pantheon:dev-8.x-1.x#$CIRCLE_SHA1
 
-# Make sure submodules are not committed.
-rm -rf modules/search_api_solr/.git/
-rm -rf modules/search_api/.git/
-rm -rf modules/search_api_page/.git/
-rm -rf modules/search_api_pantheon/.git/
-rm -rf vendor/solarium/solarium/.git/
 
 # Make a git commit
-git add .
-git commit -m 'Result of build step'
-git push --set-upstream origin $TERMINUS_ENV
+#git add .
+#git commit -m 'Result of build step'
+#git push --set-upstream origin $TERMINUS_ENV
