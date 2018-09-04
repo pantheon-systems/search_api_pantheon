eec769e0a65f31ed29d2df396728934b05f6ef9a
Steve Persch <steve.persch@pantheon.io>
2018-09-04 14:21:06 -0500

    pull in upstream updates

--- .circleci/create-fresh-d8-site.sh
+++ .circleci/create-fresh-d8-site.sh
@@ -2,12 +2,8 @@
 
 set -x
 
-# @todo, pull in upstream updates, wipe and install drupal.
-# Also, it might be cleaner to create an entirely new D8 site rather than making
-# multidevs off of the same one repeatedly.
-terminus env:create  $TERMINUS_SITE.dev $TERMINUS_ENV
+terminus upstream:updates:apply $TERMINUS_SITE
+terminus env:create $TERMINUS_SITE.dev $TERMINUS_ENV
 
-# @todo, this command gives an error
-# [error] You must upgrade to a business or an elite plan to use Solr.
-# Bug tracked here: https://github.com/pantheon-systems/terminus/issues/1118
-#terminus site solr enable
+# It's not really necessary to enable solr over and over.
+terminus solr:enable $TERMINUS_SITE
