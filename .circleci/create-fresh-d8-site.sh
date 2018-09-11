5ab354451a757b9436a5e20862c2605e37a9b1f1
Steve Persch <steve.persch@pantheon.io>
2018-09-11 14:03:42 -0500

    Do a site install every time

--- .circleci/create-fresh-d8-site.sh
+++ .circleci/create-fresh-d8-site.sh
@@ -3,6 +3,7 @@
 set -x
 
 terminus upstream:updates:apply $TERMINUS_SITE
+terminus drush --quiet $TERMINUS_SITE.dev -- si -y
 terminus env:create $TERMINUS_SITE.dev $TERMINUS_ENV
 
 # It's not really necessary to enable solr over and over.
