94344401669e34e936b5b7880dcbe2575b4df8b2
Nick Dickinson-Wilde <nick@nickdickinsonwilde.ca>
2020-12-01 11:56:55 -0800

    Improve ping on Pantheon system

--- src/Solarium/PantheonCurl.php
+++ src/Solarium/PantheonCurl.php
@@ -1,14 +1,12 @@
 <?php
 
-/**
- * @file
- * Override Solarium so that more options can be set before executing curl.
- */
-
 namespace Drupal\search_api_pantheon\Solarium;
 use Solarium\Core\Client\Adapter\AdapterHelper;
 use Solarium\Core\Client\Adapter\Curl;
 
+/**
+ * Override Solarium so that more options can be set before executing curl.
+ */
 class PantheonCurl extends Curl {
 
   /**
@@ -18,8 +16,13 @@ class PantheonCurl extends Curl {
     $handler = parent::createHandle($request, $endpoint);
     if (defined('PANTHEON_ENVIRONMENT')) {
       $uri = AdapterHelper::buildUri($request, $endpoint);
-      $uri = str_replace('/solr/.', '', $uri);
+
+      // Adjust the url from the default calculation. Remove the new url type
+      // 'solr/.' and change the ping url.
+      $uri = str_replace(['/solr/.', 'admin/ping'], ['', 'admin/system'], $uri);
       curl_setopt($handler, CURLOPT_URL, $uri);
+
+      // Set access options, use SSL Certificate.
       curl_setopt($handler, CURLOPT_SSL_VERIFYPEER, FALSE);
       $client_cert = $_SERVER['HOME'] . '/certs/binding.pem';
       curl_setopt($handler, CURLOPT_SSLCERT, $client_cert);
