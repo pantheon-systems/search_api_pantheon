b48e779a27300132849d0a5bfc5f42799ddf147d
Phil Tyler <phil.tyler@pantheon.io>
2024-11-13 22:14:10 -0800

    Revert "restore reload_path"
    This reverts commit f89aa7cbf90bbc5fe6c149237ea630a2978994aa.

--- src/Plugin/SolrConnector/PantheonSolrConnector.php
+++ src/Plugin/SolrConnector/PantheonSolrConnector.php
@@ -129,7 +129,6 @@ class PantheonSolrConnector extends SolrConnectorPluginBase implements
       'path' => getenv('PANTHEON_INDEX_PATH'),
       'core' => getenv('PANTHEON_INDEX_CORE'),
       'schema' => getenv('PANTHEON_INDEX_SCHEMA'),
-      'reload_path' => getenv('PANTHEON_INDEX_RELOAD_PATH'),
     ];
   }
 
