6fa18532ad50a8fbc1928b9a4e3c1e63912e93de
Tom Stovall <tom.stovall@pantheon.io>
2024-11-14 05:17:56 -0800

    adding form alter

---
+++ src/Plugin/FormAlter/SolrReloadFormAlter.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\search_api_pantheon\Plugin\FormAlter;
+
+/**
+ * Class SolrReloadFormAlter.
+ *
+ * @FormAlter(
+ *   id = "search_api_pantheon_reload_form_alter",
+ *   label = @Translation("Alter the reload form for the pantheon environment."),
+ *   form_id = {
+ *    "solr_reload_core_form"
+ *   },
+ * )
+ *
+ * @package Drupal\search_api_pantheon\Plugin\FormAlter
+ */
+class SolrReloadFormAlter extends FormAlterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formAlter(array &$form, FormStateInterface $form_state, $form_id) {
+    // do something here, for example add submit handler.
+    print_r($form_state->getValues());
+    $submit_handler = __CLASS__ . '::formSubmit';
+    array_unshift($form['actions']['submit']['#submit'], $submit_handler);
+  }
+
+  /**
+   * Custom form submit.
+   */
+  public static function formSubmit($form, FormStateInterface $form_state) {
+    print_r($form_state->getValues());
+    exit(1);
+  }
+
+}
