25147418b0453fff6942740972d05d9269c7cd1a
Karoly Negyesi <chx1975@gmail.com>
2025-05-02 19:41:46 +0200

    back to indexed but clean

--- .github/workflows/ci.sh
+++ .github/workflows/ci.sh
@@ -16,7 +16,8 @@ terminus workflow:wait --max=260 $SITE.dev
 terminus drush -y $SITE.dev si standard
 terminus drush -y $SITE.dev en search_api_pantheon,devel_generate
 terminus drush $SITE.dev genc 5
-if terminus drush $SITE.dev sapi-s | grep -q 'primary.*5'; then
+indexed=$(terminus drush sap-old-is-new.dev -- sapi-s primary --fields=total --format=string)
+if [ "$indexed" == "5" ];
   exit 0
 else
   echo "Not found the expected five documents, running the search-api-pantheon-diagnose command"
