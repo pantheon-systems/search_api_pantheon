eb66535d6e11efc1475dfaabaf5631994532eaf4
Karoly Negyesi <chx1975@gmail.com>
2025-05-02 19:50:49 +0200

    restore workflow_dispatch

--- .github/workflows/ci.sh
+++ .github/workflows/ci.sh
@@ -16,8 +16,8 @@ terminus workflow:wait --max=260 $SITE.dev
 terminus drush -y $SITE.dev si standard
 terminus drush -y $SITE.dev en search_api_pantheon,devel_generate
 terminus drush $SITE.dev genc 5
-indexed=$(terminus drush sap-old-is-new.dev -- sapi-s primary --fields=total --format=string)
-if [ "$indexed" == "5" ];
+indexed=$(terminus drush $SITE.dev -- sapi-s primary --fields=total --format=string)
+if [ "$indexed" == "5" ]; then
   exit 0
 else
   echo "Not found the expected five documents, running the search-api-pantheon-diagnose command"
