1c13df9054cf4566cce81772c5843256448e9e63
Tunde Afolabi <Babatundeafolabi@pantheon.io>
2026-09-25 11:52:55 -0400

    codacy issue fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,8 +13,9 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
  * Stands in for pantheon_curl_setup() from the Pantheon platform PHP prepend.
  *
  * On Pantheon this function is always defined before Drupal loads. Here it
- * returns whatever curl options the running test has told it to.
+ * ignores the arguments it is called with and returns whatever curl options
+ * the running test has told it to.
  */
-function pantheon_curl_setup($url, $data = NULL, $port = 443, $verb = 'GET') {
+function pantheon_curl_setup(): array {
   return [NULL, PantheonSolrCurlTest::$prependCurlOptions];
 }
